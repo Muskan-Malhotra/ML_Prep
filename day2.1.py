de49e64cbd1dabb7b217c1847c405f82284631b9
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-09-20 12:29:28 +0530

    Task 3

--- day2.1.py
+++ day2.1.py
@@ -1,27 +1,27 @@
-data={}
-data = [{"name":"apple","Price":"500"},{"name":"banana","price":"1500"}]
+# data={}
+# data = [{"name":"apple","Price":"500"},{"name":"banana","price":"1500"}]
 
 
-name=[]
-price=[]
-for i in data:
-#{​​'name':'bitcoin','price':500}​​
-  for j in i:
-#'name','price'
-    if j == 'name':
-      name.append(i[j])
-    else:
-      if i[j].isnumeric():
-        price.append(int(i[j]))
+# name=[]
+# price=[]
+# for i in data:
+# #{​​'name':'bitcoin','price':500}​​
+#   for j in i:
+# #'name','price'
+#     if j == 'name':
+#       name.append(i[j])
+#     else:
+#       if i[j].isnumeric():
+#         price.append(int(i[j]))
 
 
-print(name)
-print(price)
+# print(name)
+# print(price)
 
-lst = ['51','52','53']
-for i in lst:
-  if i.isnumeric():  #return true if it is a numeric string
-    print(i)
+# lst = ['51','52','53']
+# for i in lst:
+#   if i.isnumeric():  #return true if it is a numeric string
+#     print(i)
 
 
 '''
@@ -46,3 +46,43 @@ names=['john thomas','Sam','Lily','JOHN','Jonas Mark','Peter','John','Lee']
 for i in names:
   if i.startswith('j') or i.startswith('J'):
     print(i)
+
+######### Exercise 2 #########
+task = 'welcome python and JOY class'
+
+for i in range(len(task)):
+  if(task[i].lower()=='o'):
+    print(i)
+
+
+########## armstrong number ##############
+num = input("Enter the number: ")
+temp = int(num)
+lgt = len(list(num))
+
+ans = 0
+while(temp>0):
+  rem = temp%10
+  temp = temp//10  ##--> changes to 
+  ans = ans+rem**lgt
+
+if(ans == int(num)):
+  print("Is an armstrong number")
+else:
+  print("Not an armstrong number")
+
+#############part 2 ################
+num = input("Enter the number: ")
+
+n = len(num) #3
+res=0
+for i in num:
+  res = int(i)**n+res
+
+if(res == int(num)):
+  print("%s Is an armstrong number" %num)
+else:
+  print("%s Not an armstrong number" %num)
+
+
+
--- day2.py
+++ day2.py
@@ -1,14 +1,19 @@
 name = input("Enter your name: ")
+fname,lname = name.split()
 acc = input("Enter your account: ")
 add= input("Enter your address: ")
 
-fname = name.split(" ")
+firstname = name.split(" ")
+pswd = fname[::-1]
 psw = fname[0][::-1]
 
 t1=(name,acc,add,psw)
 
 print(t1)
-
+t2 = list(t1)
+t2[3]=input("New address: ")
+t1 = tuple(t2)
+print(t1)
 
 '''
 Enter your name: John Newman
